7c26f82e25717885d65644d12d173f61d5bdd1eb
Ilya <matiz86@mail.ru>
2024-03-30 09:42:24 +0300

    add location js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,33 @@
-const citySelect = document.querySelector('#city');
-const cityChoices = new Choices(citySelect, {
-  searchEnabled: false,
-  itemSelectText: '',
-});
+const API_URL = 'https://workspace-methed.vercel.app/';
+const LOCATION_URL = 'api/locations';
+
+const getData = async (url, cbSuccess, cbError) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    cbSuccess(data);
+  } catch (err) {
+    cbError(err);
+  }
+};
+
+const init = () => {
+  const citySelect = document.querySelector('#city');
+  const cityChoices = new Choices(citySelect, {
+    itemSelectText: '',
+  });
+
+  getData(
+    `${API_URL}${LOCATION_URL}`,
+    (locationData) => {
+      const location = locationData.map((location) => ({
+        value: location,
+      }));
+      cityChoices.setChoices(location, 'value', 'label', true);
+    },
+    (err) => {
+      console.log(err);
+    }
+  );
+};
+init();
